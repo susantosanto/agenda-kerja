2e62394aa67941425b516157b92c04aa369791c4
santo14 <susantosanto290687@gmail.com>
2026-05-08 10:30:40 +0700

    chore: remove eslint config to fix Vercel build error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,1 @@
-export default [
-  {
-    ignores: [".next/**", "out/**", "build/**", "public/uploads/**"],
-  },
-  {
-    rules: {
-      "react-hooks/exhaustive-deps": "off",
-      "@next/next/no-img-element": "off",
-      "no-unused-vars": "off",
-      "no-explicit-any": "off",
-    },
-  },
-]
+export default []
